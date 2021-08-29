c8bb12132df8cc5369b87d4f7e89f870dbfd89a5
Sonrak141 <cborrero094@gmail.com>
2021-08-29 18:40:40 -0500

    Desafio 6

--- desafio6/productosAPI.ts
+++ desafio6/productosAPI.ts
@@ -2,14 +2,22 @@ const express = require('express');
 const emoji = require('node-emoji');
 const {Server: HttpServer} = require('http');
 const {Server: IOServer} = require('socket.io');
+const productList =require('./class/class');
+
 
 const app = express();
 const httpServer = new HttpServer(app);
 const io = new IOServer(httpServer);
 
-io.on('connection', (socket)=>{
+const prod = new productList('./productos.txt')
+
+
+
+io.on('connection', async (socket)=>{
+    const data = await prod.read();
     console.log(emoji.get('pizza'),'Usuario Conectado')
     socket.emit('connectionMessage', 'Bienvenidos a el Socket')
+    socket.emit('Product List', data)
     socket.on('disconnect', ()=>{
         console.log(emoji.get('fire'),'Usuario Desconectado')
     })
