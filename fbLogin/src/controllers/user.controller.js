f61361fc8720a1a1072beb6e2ee042c0cc69586f
Sonrak141 <cborrero094@gmail.com>
2022-01-16 22:24:26 -0500

    fb Login desafio

--- fbLogin/src/controllers/user.controller.js
+++ fbLogin/src/controllers/user.controller.js
@@ -1,10 +1,12 @@
 import * as UserService from '../services/user.service.js'
 
+
+
 export async function createUser(req, res){
     try {
         const {body} = req;
         const response = await UserService.createUser(body)
-        res.status(200).json(response)
+       
     } catch (error) {
         res.status(400).json(error.message);
     }
